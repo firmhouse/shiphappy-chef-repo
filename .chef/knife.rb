e9acef56c6ccffddb516a7dae1345741be8a0e82
Michiel Sikkes <michiel.sikkes@gmail.com>
2012-03-26 21:50:06 +0200

    Updated chef configuration

--- .chef/knife.rb
+++ .chef/knife.rb
@@ -1,3 +1,6 @@
 current_dir = File.dirname(__FILE__)
 cookbook_path ["#{current_dir}/../cookbooks"]
 
+cookbook_license :apachev2
+cookbook_email "michiel@firmhouse.com"
+cookbook_copyright "Michiel Sikkes" 
