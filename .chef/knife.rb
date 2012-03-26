3ce3b380a4d9c67e3a0205a2f46fb2a7cc4d8c98
Michiel Sikkes <michiel.sikkes@gmail.com>
2012-03-26 21:52:08 +0200

    Ignore correct files

--- .chef/knife.rb
+++
@@ -1,6 +0,0 @@
-current_dir = File.dirname(__FILE__)
-cookbook_path ["#{current_dir}/../cookbooks"]
-
-cookbook_license :apachev2
-cookbook_email "michiel@firmhouse.com"
-cookbook_copyright "Michiel Sikkes" 
